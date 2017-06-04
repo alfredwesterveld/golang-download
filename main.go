008304c74195a8582c7710e1c933f4a90fdcb8d8
Alfred Westerveld <alfredwesterveld@gmail.com>
2017-06-04 14:19:29 +0200

    Read line by line from stdin

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-    "time"
+    "bufio"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
-    "net/url"
+	"net/url"
 	"os"
 	"strings"
+	"time"
 )
 
 func check(e error) {
@@ -17,33 +18,41 @@ func check(e error) {
 	}
 }
 
-func parseUrl(s string) string{
-    u, _ := url.Parse(s)
-    return u.Path[1:]
+func parseUrl(s string) string {
+	u, _ := url.Parse(s)
+	return u.Path[1:]
 }
 
 func download(_url string, filename string) {
 	img, _ := os.Create(filename)
-    resp, _ := http.Get(_url)
-    io.Copy(img, resp.Body)
-	defer img.Close()
+	resp, _ := http.Get(_url)
+
+    defer img.Close()
 	defer resp.Body.Close()
+
+    io.Copy(img, resp.Body)
 }
 
-func ns() {
-    now := time.Now()
-    fmt.Println(now)
+func readStdin() {
+    scanner := bufio.NewScanner(os.Stdin)
+
+    for scanner.Scan() {
+        line := scanner.Text()
+        download(line, parseUrl(line))
+    }
 }
 
-func main() {
+func downloadFile() {
 	content, _ := ioutil.ReadFile("./urls.txt")
 	lines := strings.Split(string(content), "\n")
 
-	for _, v := range lines {
+    for _, v := range lines {
 		if v != "" {
-            download(v, parseUrl(v))
-            // println(parseUrl(v))
-            // download(v)
+			download(v, parseUrl(v))
 		}
 	}
+}
+
+func main() {
+	readStdin()
 }
